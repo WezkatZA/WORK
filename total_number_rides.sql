0f0c41a25ea364192c321b9c5c9de8896ffef5d9
WezkatZA <98463446+WezkatZA@users.noreply.github.com>
2022-02-16 14:19:47 +0100

    Delete total_number_rides.sql

--- total_number_rides.sql
+++
@@ -1,7 +0,0 @@
-/****** Total number of riders throughout the year  ******/
-SELECT 
-	COUNT([ride_id]) AS num_riders, 
-	member_casual
-
-FROM [Cyclistic].[dbo].[rides_2021]  
-GROUP BY member_casual
