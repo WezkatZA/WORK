6888fe991bb448f84723e9ec453e9d6af33d462a
WezkatZA <98463446+WezkatZA@users.noreply.github.com>
2022-02-16 14:20:02 +0100

    Delete mode_day.sql

--- mode_day.sql
+++
@@ -1,9 +0,0 @@
-/****** Find out the most popular day overall according to member status  ******/
-SELECT COUNT([ride_id]) AS num_riders
-     
-      ,[member_casual]
-      
-      ,[day_of_week]
-  FROM [Cyclistic].[dbo].[rides_2021]
-  GROUP BY day_of_week, member_casual
-  ORDER BY day_of_week, member_casual
